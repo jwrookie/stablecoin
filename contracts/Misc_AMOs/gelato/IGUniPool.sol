45577741df8dc177de4dfa41242c0a0618d7baa1
defi <defiiron@gmail.com>
2022-03-05 14:27:28 +0800

    Remove uniswap v3

--- contracts/Misc_AMOs/gelato/IGUniPool.sol
+++ contracts/Misc_AMOs/gelato/IGUniPool.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity >=0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { IUniswapV3Pool } from "../../Uniswap_V3/IUniswapV3Pool.sol";
+import { IUniswapV3Pool } from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 
 interface IGUniPool {
     function mint(uint256 mintAmount, address receiver)
