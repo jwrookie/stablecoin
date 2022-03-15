a25f8fbfc86b2157f901e3329affc56e40566645
“xie20200914” <lingfengyisishui@163.com>
2022-03-15 18:02:48 +0800

    test setETHUSDOracle

---
+++ scripts/oracle.js
@@ -0,0 +1,107 @@
+const {expectRevert, time} = require('@openzeppelin/test-helpers');
+const TestOwnableToken = artifacts.require('TestOwnableToken');
+// const Timelock = artifacts.require('Timelock');
+const FRAXShares = artifacts.require('FRAXShares');
+const FRAXStablecoin = artifacts.require('FRAXStablecoin');
+const Pool_USDC = artifacts.require('Pool_USDC');
+const TestERC20 = artifacts.require('TestERC20');
+const TestOracle = artifacts.require('TestOracle')
+const FraxPoolLibrary = artifacts.require('FraxPoolLibrary');
+const {toWei} = web3.utils;
+
+// const {BigNumber} = require('ethers');
+
+//const Timelock = require('../contracts/tools/Timelock.json');
+const {deployContract, MockProvider, solidity, Fixture} = require('ethereum-waffle');
+const {ethers, waffle} = require("hardhat");
+
+
+
+function encodeParameters(types, values) {
+    const abi = new ethers.utils.AbiCoder();
+    return abi.encode(types, values);
+}
+
+async function main() {
+    const accounts = await ethers.getSigners()
+    const zeroAddr = "0x0000000000000000000000000000000000000000"
+    // let usdc = "0x42d903C9503c22a83a92e216F088d8BaDff0c699"
+    let dai = "0xD4EDbFcDB6E5eBFA20e09a1B216ca5c84e4Ad889"
+
+let usdt = "0xfecaB3217751C1c92301F827e309ec552100dAC1"
+    let timeLock = "0x4e5726cB91A518B55c02E67822925e947bE56F46"
+
+    for (const account of accounts) {
+        //console.log('Account address' + account.address)
+    }
+
+    let deployer = accounts[0]
+    console.log('deployer:' + deployer.address)
+    // We get the contract to deploy
+    console.log('Account balance:', (await deployer.getBalance()).toString()/10**18)
+
+
+
+
+   // timeLock = await deployContract(deployer, {
+   //      bytecode: Timelock.bytecode,
+   //      abi: Timelock.abi
+   //  }, [deployer.address, 0]);
+   //  console.log("timeLock:" + timeLock.address)
+
+
+    // const TimeLock = await ethers.getContractFactory("TimeLock");
+    // timeLock = await TimeLock.deploy(deployer.address, 0);
+    // console.log("timeLock:" + timeLock.address);
+
+    //    const UniV2TWAMMFactory = await ethers.getContractFactory("UniV2TWAMMFactory");
+    // factory = await UniV2TWAMMFactory.deploy(deployer.address);
+    // console.log("factory:" + factory.address);
+    //
+    //  const UniswapPairOracle = await ethers.getContractFactory("UniswapPairOracle");
+    // usdc = await UniswapPairOracle.deploy(factory.address,a,b,deployer.address,timeLock);
+    // await factory.createPair(dai,usdc)
+    // console.log("usdc:" + usdc.address);
+
+    // const FRAXShares = await ethers.getContractFactory("FRAXShares");
+    // fxs = await FRAXShares.deploy("Test","Test",oracle);
+    // console.log("fxs:" + fxs.address);
+    //
+    // const FRAXStablecoin = await ethers.getContractFactory("FRAXStablecoin");
+    // frax = await FRAXStablecoin.deploy( "usdc", "usdc");
+    // console.log("frax:" + frax.address);
+
+    // const ChainlinkETHUSDPriceConsumer = await ethers.getContractFactory("ChainlinkETHUSDPriceConsumer");
+    // chainlinkETHUSDPriceConsumer = await ChainlinkETHUSDPriceConsumer.deploy();
+    // console.log("chainlinkETHUSDPriceConsumer:" + chainlinkETHUSDPriceConsumer.address);
+
+     const ChainlinkFXSUSDPriceConsumer = await ethers.getContractFactory("ChainlinkFXSUSDPriceConsumer");
+    chainlinkFXSUSDPriceConsumer = await ChainlinkFXSUSDPriceConsumer.deploy();
+    console.log("chainlinkFXSUSDPriceConsumer:" + chainlinkFXSUSDPriceConsumer.address);
+
+
+    // const FraxPoolLibrary = await ethers.getContractFactory("FraxPoolLibrary");
+    // fraxPoolLibrary = await FraxPoolLibrary.deploy();
+    // console.log("fraxPoolLibrary:" + fraxPoolLibrary.address);
+    //
+    //
+    // const Pool_USDC = await ethers.getContractFactory('Pool_USDC', {
+    //         libraries: {
+    //             FraxPoolLibrary: fraxPoolLibrary.address,
+    //         },
+    //     });
+    // pool = await Pool_USDC.deploy(frax,fxs,usdc,100);
+    // console.log("pool:" + pool.address);
+
+
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
--- test/frax.test.js
+++ test/frax.test.js
@@ -1,14 +1,5 @@
 const {expectRevert, time} = require('@openzeppelin/test-helpers');
 const {ethers, waffle} = require("hardhat");
-const TestOwnableToken = artifacts.require('TestOwnableToken');
-const FraxPoolLibrary = artifacts.require('FraxPoolLibrary');
-const Timelock = artifacts.require('Timelock');
-const FRAXShares = artifacts.require('FRAXShares');
-const FRAXStablecoin = artifacts.require('FRAXStablecoin');
-// const Pool_USDC = artifacts.require('Pool_USDC');
-const TestERC20 = artifacts.require('TestERC20');
-const TestOracle = artifacts.require('TestOracle')
-
 
 function encodeParameters(types, values) {
     const abi = new ethers.utils.AbiCoder();
@@ -17,42 +8,56 @@ function encodeParameters(types, values) {
 
 contract('FRAXStablecoin', ([owner, alice, bob, carol]) => {
     beforeEach(async () => {
-        usdc = await TestERC20.new();
-        oracle = await TestOracle.new();
+        const TestERC20 = await ethers.getContractFactory('TestERC20');
+        usdc = await TestERC20.deploy();
 
-        timelock = await Timelock.new(owner, '259200');
-        zeroAddress = "0x";
-        fxs = await FRAXShares.new("Test", "Test", oracle.address)
+        const TestOracle = await ethers.getContractFactory('TestOracle');
+        oracle = await TestOracle.deploy();
 
-        frax = await FRAXStablecoin.new("usdc", "usdc");
+        const Timelock = await ethers.getContractFactory('Timelock');
+        timelock = await Timelock.deploy(owner, "259200");
+
+        const FRAXShares = await ethers.getContractFactory('FRAXShares');
+        fxs = await FRAXShares.deploy("Test", "Test", oracle.address);
+
+        const FRAXStablecoin = await ethers.getContractFactory('FRAXStablecoin');
+        frax = await FRAXStablecoin.deploy("usdc", "usdc");
         await fxs.setFraxAddress(frax.address);
+        await frax.setFXSAddress(fxs.address);
 
         assert.equal(await fxs.oracle(), oracle.address);
-        // assert.equal(await frax.fxsAddress(),fxs.address);
-        //   console.log("")
-        fraxPoolLibrary = await FraxPoolLibrary.new();
-        // await Pool_USDC.link( fraxPoolLibrary.address)
+        assert.equal(await frax.fxsAddress(), fxs.address);
 
-        Pool_USDC= await ethers.getContractFactory('Pool_USDC', {
+        const FraxPoolLibrary = await ethers.getContractFactory('FraxPoolLibrary')
+        fraxPoolLibrary = await FraxPoolLibrary.deploy();
+
+        const Pool_USDC = await ethers.getContractFactory('Pool_USDC', {
             libraries: {
                 FraxPoolLibrary: fraxPoolLibrary.address,
             },
         });
+        pool = await Pool_USDC.deploy(frax.address, fxs.address, usdc.address, 100);
+        assert.equal(await pool.USDC_address(), usdc.address)
 
-
-
-        pool  = await Pool_USDC.deploy(frax.address,fxs.address,usdc.address,100);
-
+        const ChainlinkETHUSDPriceConsumer = await ethers.getContractFactory("ChainlinkETHUSDPriceConsumer");
+        chainlinkETHUSDPriceConsumer = await ChainlinkETHUSDPriceConsumer.deploy();
 
 
     });
 
-    it('test', async () => {
-        console.log("fxs:" + fxs.address)
-        console.log("stablecoin:" + frax.address)
+    it('test ', async () => {
+        await frax.setETHUSDOracle(chainlinkETHUSDPriceConsumer.address)
 
-
-        console.log("pool:"+pool.address)
+        // assert.equal(await frax.isFraxPools(pool.address), false)
+        // await frax.addPool(pool.address);
+        // assert.equal(await frax.isFraxPools(pool.address), true)
+        // //await fxs.poolMint(owner,1000);
+        // await frax.mint(owner,1000);
+        // console.log("frax:" + await frax.balanceOf(owner))
+        // console.log("fxs:" + await fxs.balanceOf(owner))
+        // // // await fxs.mint(owner,1000)
+        // //  console.log("fxs:"+await fxs.balanceOf(owner))
+        // console.log("usdc:" + await usdc.balanceOf(owner))
 
 
     });
