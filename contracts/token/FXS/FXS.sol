dfe9e1b2a4353f328f682883fe50ebd9bc3f479e
defi <defiiron@gmail.com>
2022-03-15 09:35:05 +0800

    Remove unuse code

--- contracts/token/FXS/FXS.sol
+++ contracts/token/FXS/FXS.sol
@@ -1,26 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity >=0.6.11;
 
-// ====================================================================
-// |     ______                   _______                             |
-// |    / _____________ __  __   / ____(_____  ____ _____  ________   |
-// |   / /_  / ___/ __ `| |/_/  / /_  / / __ \/ __ `/ __ \/ ___/ _ \  |
-// |  / __/ / /  / /_/ _>  <   / __/ / / / / / /_/ / / / / /__/  __/  |
-// | /_/   /_/   \__,_/_/|_|  /_/   /_/_/ /_/\__,_/_/ /_/\___/\___/   |
-// |                                                                  |
-// ====================================================================
-// ========================= FRAXShares (FXS) =========================
-// ====================================================================
-// Frax Finance: https://github.com/FraxFinance
-
-// Primary Author(s)
-// Travis Moore: https://github.com/FortisFortuna
-// Jason Huan: https://github.com/jasonhuan
-// Sam Kazemian: https://github.com/samkazemian
-
-// Reviewer(s) / Contributor(s)
-// Sam Sun: https://github.com/samczsun
-
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
@@ -41,22 +21,7 @@ contract FRAXShares is ERC20Burnable, AbstractPausable {
     uint256 public constant genesis_supply = 100000000e18; // 100M is printed upon genesis
 
     address public oracle_address;
-    address public timelock_address; // Governance timelock address
     FRAXStablecoin private FRAX;
-
-    bool public trackingVotes = true; // Tracking votes (only change if need to disable votes)
-
-    // A checkpoint for marking number of votes from a given block
-    struct Checkpoint {
-        uint32 fromBlock;
-        uint96 votes;
-    }
-
-    // A record of votes checkpoints for each account, by index
-    mapping(address => mapping(uint32 => Checkpoint)) public checkpoints;
-
-    // The number of checkpoints for each account
-    mapping(address => uint32) public numCheckpoints;
 
     /* ========== MODIFIERS ========== */
 
@@ -85,11 +50,6 @@ contract FRAXShares is ERC20Burnable, AbstractPausable {
         oracle_address = new_oracle;
     }
 
-    function setTimelock(address new_timelock) external onlyOwner {
-        require(new_timelock != address(0), "Timelock address cannot be 0");
-        timelock_address = new_timelock;
-    }
-
     function setFRAXAddress(address frax_contract_address) external onlyOwner {
         require(frax_contract_address != address(0), "Zero address detected");
 
@@ -104,55 +64,25 @@ contract FRAXShares is ERC20Burnable, AbstractPausable {
 
     // This function is what other frax pools will call to mint new FXS (similar to the FRAX mint) 
     function pool_mint(address m_address, uint256 m_amount) external onlyPools {
-        if (trackingVotes) {
-            uint32 srcRepNum = numCheckpoints[address(this)];
-            uint96 srcRepOld = srcRepNum > 0 ? checkpoints[address(this)][srcRepNum - 1].votes : 0;
-            uint96 srcRepNew = add96(srcRepOld, uint96(m_amount), "pool_mint new votes overflows");
-            _writeCheckpoint(address(this), srcRepNum, srcRepOld, srcRepNew);
-            // mint new votes
-            trackVotes(address(this), m_address, uint96(m_amount));
-        }
-
         super._mint(m_address, m_amount);
         emit FXSMinted(address(this), m_address, m_amount);
     }
 
     // This function is what other frax pools will call to burn FXS 
     function pool_burn_from(address b_address, uint256 b_amount) external onlyPools {
-        if (trackingVotes) {
-            trackVotes(b_address, address(this), uint96(b_amount));
-            uint32 srcRepNum = numCheckpoints[address(this)];
-            uint96 srcRepOld = srcRepNum > 0 ? checkpoints[address(this)][srcRepNum - 1].votes : 0;
-            uint96 srcRepNew = sub96(srcRepOld, uint96(b_amount), "pool_burn_from new votes underflows");
-            _writeCheckpoint(address(this), srcRepNum, srcRepOld, srcRepNew);
-            // burn votes
-        }
 
         super.burnFrom(b_address, b_amount);
         emit FXSBurned(b_address, address(this), b_amount);
     }
 
-    function toggleVotes() external onlyOwner {
-        trackingVotes = !trackingVotes;
-    }
-
     /* ========== OVERRIDDEN PUBLIC FUNCTIONS ========== */
 
     function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
-        if (trackingVotes) {
-            // Transfer votes
-            trackVotes(_msgSender(), recipient, uint96(amount));
-        }
-
         _transfer(_msgSender(), recipient, amount);
         return true;
     }
 
     function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
-        if (trackingVotes) {
-            // Transfer votes
-            trackVotes(sender, recipient, uint96(amount));
-        }
 
         _transfer(sender, recipient, amount);
         _approve(sender, _msgSender(), allowance(sender, _msgSender()).sub(amount, "ERC20: transfer amount exceeds allowance"));
@@ -160,104 +90,6 @@ contract FRAXShares is ERC20Burnable, AbstractPausable {
         return true;
     }
 
-    /* ========== PUBLIC FUNCTIONS ========== */
-
-    /**
-     * @notice Gets the current votes balance for `account`
-     * @param account The address to get votes balance
-     * @return The number of current votes for `account`
-     */
-    function getCurrentVotes(address account) external view returns (uint96) {
-        uint32 nCheckpoints = numCheckpoints[account];
-        return nCheckpoints > 0 ? checkpoints[account][nCheckpoints - 1].votes : 0;
-    }
-
-    /**
-     * @notice Determine the prior number of votes for an account as of a block number
-     * @dev Block number must be a finalized block or else this function will revert to prevent misinformation.
-     * @param account The address of the account to check
-     * @param blockNumber The block number to get the vote balance at
-     * @return The number of votes the account had as of the given block
-     */
-    function getPriorVotes(address account, uint blockNumber) public view returns (uint96) {
-        require(blockNumber < block.number, "FXS::getPriorVotes: not yet determined");
-
-        uint32 nCheckpoints = numCheckpoints[account];
-        if (nCheckpoints == 0) {
-            return 0;
-        }
-
-        // First check most recent balance
-        if (checkpoints[account][nCheckpoints - 1].fromBlock <= blockNumber) {
-            return checkpoints[account][nCheckpoints - 1].votes;
-        }
-
-        // Next check implicit zero balance
-        if (checkpoints[account][0].fromBlock > blockNumber) {
-            return 0;
-        }
-
-        uint32 lower = 0;
-        uint32 upper = nCheckpoints - 1;
-        while (upper > lower) {
-            uint32 center = upper - (upper - lower) / 2;
-            // ceil, avoiding overflow
-            Checkpoint memory cp = checkpoints[account][center];
-            if (cp.fromBlock == blockNumber) {
-                return cp.votes;
-            } else if (cp.fromBlock < blockNumber) {
-                lower = center;
-            } else {
-                upper = center - 1;
-            }
-        }
-        return checkpoints[account][lower].votes;
-    }
-
-    /* ========== INTERNAL FUNCTIONS ========== */
-
-    // From compound's _moveDelegates
-    // Keep track of votes. "Delegates" is a misnomer here
-    function trackVotes(address srcRep, address dstRep, uint96 amount) internal {
-        if (srcRep != dstRep && amount > 0) {
-            if (srcRep != address(0)) {
-                uint32 srcRepNum = numCheckpoints[srcRep];
-                uint96 srcRepOld = srcRepNum > 0 ? checkpoints[srcRep][srcRepNum - 1].votes : 0;
-                uint96 srcRepNew = sub96(srcRepOld, amount, "FXS::_moveVotes: vote amount underflows");
-                _writeCheckpoint(srcRep, srcRepNum, srcRepOld, srcRepNew);
-            }
-
-            if (dstRep != address(0)) {
-                uint32 dstRepNum = numCheckpoints[dstRep];
-                uint96 dstRepOld = dstRepNum > 0 ? checkpoints[dstRep][dstRepNum - 1].votes : 0;
-                uint96 dstRepNew = add96(dstRepOld, amount, "FXS::_moveVotes: vote amount overflows");
-                _writeCheckpoint(dstRep, dstRepNum, dstRepOld, dstRepNew);
-            }
-        }
-    }
-
-    function _writeCheckpoint(address voter, uint32 nCheckpoints, uint96 oldVotes, uint96 newVotes) internal {
-        uint32 blockNumber = safe32(block.number, "FXS::_writeCheckpoint: block number exceeds 32 bits");
-
-        if (nCheckpoints > 0 && checkpoints[voter][nCheckpoints - 1].fromBlock == blockNumber) {
-            checkpoints[voter][nCheckpoints - 1].votes = newVotes;
-        } else {
-            checkpoints[voter][nCheckpoints] = Checkpoint(blockNumber, newVotes);
-            numCheckpoints[voter] = nCheckpoints + 1;
-        }
-
-        emit VoterVotesChanged(voter, oldVotes, newVotes);
-    }
-
-    function safe32(uint n, string memory errorMessage) internal pure returns (uint32) {
-        require(n < 2 ** 32, errorMessage);
-        return uint32(n);
-    }
-
-    function safe96(uint n, string memory errorMessage) internal pure returns (uint96) {
-        require(n < 2 ** 96, errorMessage);
-        return uint96(n);
-    }
 
     function add96(uint96 a, uint96 b, string memory errorMessage) internal pure returns (uint96) {
         uint96 c = a + b;
