1c0b3a5b14090ec423c566d65d96386e8046f256
defi <defiiron@gmail.com>
2022-03-17 12:10:26 +0800

    Mint and redeem

--- contracts/token/FXB/FraxBondIssuer.sol
+++ contracts/token/FXB/FraxBondIssuer.sol
@@ -18,6 +18,7 @@ contract FraxBondIssuer is AbstractPausable {
     enum DirectionChoice {BELOW_TO_PRICE_FRAX_IN, ABOVE_TO_PRICE}
 
 
+    uint256 public constant ONE_YEAR = 1 * 365 * 86400;
     uint256 public constant PRICE_PRECISION = 1e6;
     uint256 private constant PRICE_PRECISION_SQUARED = 1e12;
     uint256 private constant PRICE_PRECISION_SQRT = 1e3;
@@ -25,10 +26,19 @@ contract FraxBondIssuer is AbstractPausable {
     FRAXStablecoin public FRAX;
     FraxBond public FXB;
 
+    uint256 public lastInterestTime;
+    uint256 public exchangeRate;
+    uint256 public interestRate;
 
-    // Minimum cooldown period before a new epoch, in seconds
-    // Bonds should be redeemed during this time, or they risk being rebalanced with a new epoch
-    uint256 public cooldownPeriod = 864000; // 10 days
+    function calInterest() public {
+
+        if (block.timestamp > lastInterestTime) {
+            uint256 timePast = block.timestamp.sub(lastInterestTime);
+            uint256 interest = interestRate.mul(timePast).div(ONE_YEAR).div(1e18);
+            exchangeRate = exchangeRate.add(interest);
+            lastInterestTime = block.timestamp;
+        }
+    }
 
     // Max FXB outstanding
     uint256 public maxFxbOutstanding = 1000000e18;
@@ -148,8 +158,6 @@ contract FraxBondIssuer is AbstractPausable {
             frax_spot_to_issue = 0;
         }
     }
-
-
 
     /* ========== PUBLIC FUNCTIONS ========== */
 
@@ -330,18 +338,21 @@ contract FraxBondIssuer is AbstractPausable {
         }
     }
 
-    function redeemFXB(uint256 fxb_in) external whenNotPaused returns (uint256 frax_out, uint256 frax_fee) {
+    function mintBond(uint256 fraxIn) external whenNotPaused returns (uint256 fxbOut, uint256 fraxFee) {
+        FRAX.poolBurnFrom(msg.sender, fraxIn);
+        fraxFee = fraxIn.mul(issueFee).div(PRICE_PRECISION);
+        fxbOut = fraxIn.mul(1e18).div(exchangeRate);
+        FXB.issuer_mint(msg.sender, fxbOut);
+        emit BondMint(msg.sender, fraxIn, fxbOut);
+    }
+
+    function redeemBond(uint256 fxbIn) external whenNotPaused returns (uint256 fraxOut, uint256 fraxFee) {
         // Burn FXB from the sender
-        FXB.burnFrom(msg.sender, fxb_in);
-
-        // Give 1 FRAX per 1 FXB, minus the redemption fee
-        frax_fee = fxb_in.mul(redemptionFee).div(PRICE_PRECISION);
-        frax_out = fxb_in.sub(frax_fee);
-
-        // Give the FRAX to the redeemer
-        FRAX.poolMint(msg.sender, frax_out);
-
-        emit FXB_Redeemed(msg.sender, fxb_in, frax_out);
+        FXB.burnFrom(msg.sender, fxbIn);
+        fraxOut = fxbIn.mul(exchangeRate).div(1e18);
+        fraxFee = fraxOut.mul(redemptionFee).div(PRICE_PRECISION);
+        FRAX.poolMint(msg.sender, fraxOut);
+        emit BondRedeemed(msg.sender, fxbIn, fraxOut);
     }
 
     /* ========== RESTRICTED INTERNAL FUNCTIONS ========== */
@@ -487,7 +498,9 @@ contract FraxBondIssuer is AbstractPausable {
     event Recovered(address token, address to, uint256 amount);
 
     // Track bond redeeming
-    event FXB_Redeemed(address indexed from, uint256 fxb_amount, uint256 frax_out);
+    event BondRedeemed(address indexed from, uint256 fxb_amount, uint256 frax_out);
+    event BondMint(address indexed from, uint256 frax_amount, uint256 fxb_out);
+    //    event FXB_Redeemed(address indexed from, uint256 fxb_amount, uint256 frax_out);
 }
 
 
