e7d83ad4639d4fc3d46e6602aaaf35a6b1bfb3b1
defi <defiiron@gmail.com>
2022-03-31 15:23:18 +0800

    Add swap mining

--- contracts/dao/LockerV2.sol
+++ contracts/dao/LockerV2.sol
@@ -3,8 +3,6 @@ pragma solidity 0.8.10;
 pragma experimental ABIEncoderV2;
 
 import "./interfaces/MathUtil.sol";
-import "./interfaces/IStakingProxy.sol";
-import "./interfaces/IRewardStaking.sol";
 import "./interfaces/BoringMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
