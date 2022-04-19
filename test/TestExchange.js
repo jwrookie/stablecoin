b9bde27786958e8014e66ee99872145b866de97d
JunkingBoy <junkingboy@163.com>
2022-04-19 15:42:21 +0800

    Test contract of ExchangeAMO error

--- test/TestExchange.js
+++ test/TestExchange.js
@@ -29,6 +29,20 @@ contract('ExchangeAMO', async function() {
     let poolRegistry;
     let plain3Balances;
     let crvFactory;
+    let exchangeAMO;
+    let frax;
+    let tempCRVPool;
+    let poolAddress;
+    let poolTwoAddress;
+    let pool;
+    let poolTwo;
+    let byteArray;
+    let usdc;
+    let oracle;
+    let fax;
+    let fraxPoolLibrary;
+    let amoMinter;
+    let initFirstPool;
 
     async function getUint8Array(len) {
         var buffer = new ArrayBuffer(len); 
@@ -42,20 +56,6 @@ contract('ExchangeAMO', async function() {
     }
 
     beforeEach(async function() {
-        let tempCRVPool;
-        let poolAddress;
-        let poolTwoAddress;
-        let pool;
-        let poolTwo;
-        let byteArray;
-        let usdc;
-        let oracle;
-        let frax;
-        let fax;
-        let fraxPoolLibrary;
-        let amoMinter;
-        let exchangeAMO;
-
         [owner, dev, addr1] = await ethers.getSigners();
 
         // Inherit ERC20
@@ -127,6 +127,9 @@ contract('ExchangeAMO', async function() {
         expect(poolAddress).to.be.not.eq(null);
         pool = await plain3Balances.attach(poolAddress);
         expect(parseInt(pool.length)).to.be.not.eq(0);
+        initFirstPool = pool;
+        expect(initFirstPool).to.be.eq(pool);
+        expect(poolAddress).to.be.eq(pool.address);
 
         // Approve
         await token0.approve(pool.address, toWei(mockTokenApproveCount));
@@ -135,7 +138,7 @@ contract('ExchangeAMO', async function() {
 
         // Add liquidity
         await pool.add_liquidity([toWei("10"), toWei("10"), toWei("10")], 0);
-
+        
         // Aonter pool
         await crvFactory.deploy_plain_pool(
             "3pool2",
@@ -155,7 +158,7 @@ contract('ExchangeAMO', async function() {
         await token2.approve(poolTwo.address, toWei(mockTokenApproveCount));
 
         // Add liquidity
-        await pool.add_liquidity([toWei("1"), toWei("9"), toWei("9")], 0);
+        await poolTwo.add_liquidity([toWei("1"), toWei("9"), toWei("9")], 0);
 
         byteArray = await getUint8Array(32);
         expect(parseInt(byteArray.length)).to.be.eq(32);
@@ -214,12 +217,104 @@ contract('ExchangeAMO', async function() {
             frax.address,
             usdc.address,
             token0.address,
-            plain3Balances.address,
+            pool.address,
             token1.address
         );
     });
 
     it('test showAllocations', async function() {
+        let allocationsArray;
 
+        // await exchangeAMO.showAllocations();
+
+        // for (var i = 0; i < allocationsArray.length; i++) {
+        //     console.log(allocationsArray[i]);
+        // }
     });
+
+    it('test usdValueInVault', async function() {
+        const targetValue = 1e18;
+        let usdValue;
+
+        usdValue = await exchangeAMO.usdValueInVault();
+        expect(parseInt(usdValue)).to.be.eq(targetValue);
+    });
+
+    it('test fraxFloor', async function() {
+        let fraxCollateralRatio;
+        let functionReturnRatio;
+
+        fraxCollateralRatio = await frax.globalCollateralRatio();
+        functionReturnRatio = await exchangeAMO.fraxFloor();
+
+        console.log(parseInt(functionReturnRatio));
+
+        expect(parseInt(functionReturnRatio)).to.be.eq(parseInt(fraxCollateralRatio));
+    });
+
+    it('test metapoolDeposit', async function() {
+        let fraxAmount;
+        let collateralAmount;
+        let metaPoolLpReceived;
+
+        fraxAmount = await frax.balanceOf(owner.address);
+        collateralAmount = await usdc.balanceOf(owner.address);
+
+        var tempArray = new Array(3);
+        // tempArray[0] = 0;
+        tempArray[1] = 1;
+        // tempArray[2] = 0;
+        var usdcD = await usdc.decimals();
+        console.log(parseInt(usdcD));
+        console.log(parseInt(await exchangeAMO.missing_decimals()));
+        var tempMinLpOut = 1000000 * (10 ** await exchangeAMO.missing_decimals()) * 800000 / 1e6;
+        console.log(parseInt(tempMinLpOut));
+
+        // var temp = await initFirstPool.add_liquidity(tempArray, 0);
+        // console.log(temp);
+
+        metaPoolLpReceived = await exchangeAMO.metapoolDeposit(toWei("1"), toWei("1"));
+        // console.log(metaPoolLpReceived);
+    });
+
+    // it('test iterate', async function() {
+    //     const mulNumber = 1e18;
+    //     const divNumber = 1e6;
+    //     let virtualPrice;
+    //     let crv3Balance;
+    //     let fraxBalance;
+    //     let totalBalance;
+    //     let fraxCollateralRatio;
+    //     let floorPriceFrax;
+    //     let frax3CrvPool;
+
+    //     frax3CrvPool = await exchangeAMO.frax3crv_metapool_address();
+
+    //     console.log(frax3CrvPool);
+
+    //     fraxBalance = await frax.balanceOf(frax3CrvPool);
+    //     crv3Balance = await token1.balanceOf(frax3CrvPool);
+    //     totalBalance = await fraxBalance.add(crv3Balance);
+
+    //     console.log("fraxBalance:\t" + fraxBalance);
+    //     console.log("crv3Balnace:\t" + crv3Balance);
+    //     console.log("totalBalance:\t" + totalBalance);
+
+    //     fraxCollateralRatio = await exchangeAMO.fraxFloor();
+
+    //     floorPriceFrax = mulNumber * fraxCollateralRatio / divNumber;
+
+    //     // virtualPrice = await initFirstPool.get_dy(0, 1, 1e18, [fraxBalance, crv3Balance]); // over flow -> 180
+
+    //     // await exchangeAMO.iterate();
+
+    //     // virtualPrice = await initFirstPool.get_virtual_price();
+    //     // console.log(parseInt(virtualPrice));
+
+    //     console.log(parseInt(await initFirstPool.fee({ gasLimit : "1250000" }))); // Call this function fail -> 181
+
+    //     // await exchangeAMO.iterate();
+    // });
+
+    it('')
 });
