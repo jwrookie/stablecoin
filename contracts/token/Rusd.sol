5bd7e75cfac3dc74c242fb4b2a71ea8ad590bbb3
defi <defiiron@gmail.com>
2022-05-04 22:21:13 +0800

    Fix test bug

--- contracts/token/Rusd.sol
+++ contracts/token/Rusd.sol
@@ -16,57 +16,53 @@ import "../tools/AbstractPausable.sol";
 contract RStablecoin is ERC20Burnable, AbstractPausable {
     using SafeMath for uint256;
 
-
-    // Track FRAX burned
-    event FRAXBurned(address indexed from, address indexed to, uint256 amount);
-
-    // Track FRAX minted
-    event FRAXMinted(address indexed from, address indexed to, uint256 amount);
+    event StableBurned(address indexed from, address indexed to, uint256 amount);
+    event StableMinted(address indexed from, address indexed to, uint256 amount);
 
     event CollateralRatioRefreshed(uint256 globalCollateralRatio);
     event PoolAdded(address pool_address);
     event PoolRemoved(address pool_address);
     event RedemptionFeeSet(uint256 red_fee);
     event MintingFeeSet(uint256 min_fee);
-    event FraxStepSet(uint256 new_step);
+    event StableStepSet(uint256 new_step);
     event PriceTargetSet(uint256 new_price_target);
     event RefreshCooldownSet(uint256 new_cooldown);
-    event FXSAddressSet(address _fxs_address);
+    event StockAddressSet(address _fxs_address);
     event ETHUSDOracleSet(address eth_usd_consumer_address);
     event TimelockSet(address new_timelock);
     event ControllerSet(address controller_address);
     event PriceBandSet(uint256 price_band);
-    event FRAXETHOracleSet(address frax_oracle_addr, address weth_address);
-    event FXSEthOracleSet(address fxs_oracle_addr, address weth_address);
+    event StableETHOracleSet(address oracle_addr, address weth_address);
+    event StockEthOracleSet(address oracle_addr, address weth_address);
 
     uint256 public constant GENESIS_SUPPLY = 2000000e18; // 2M FRAX (only for testing, genesis supply will be 5k on Mainnet). This is to help with establishing the Uniswap pools, as they need liquidity
     // Constants for various precisions
     uint256 private constant PRICE_PRECISION = 1e6;
 
-    enum PriceChoice {FRAX, FXS}
+    enum PriceChoice {STABLE, STOCK}
     ChainlinkETHUSDPriceConsumer private ethUsdPricer;
     uint8 private ethUsdPricerDecimals;
-    UniswapPairOracle private fraxEthOracle;
-    UniswapPairOracle private fxsEthOracle;
+    UniswapPairOracle private stableEthOracle;
+    UniswapPairOracle private stockEthOracle;
 
-    address public fxsAddress;
-    address public fraxEthOracleAddress;
-    address public fxsEthOracleAddress;
+    address public stockAddress;
+    address public stableEthOracleAddress;
+    address public stockEthOracleAddress;
     address public weth;
     address public ethUsdConsumerAddress;
 
 
     // The addresses in this array are added by the oracle and these contracts are able to mint frax
-    address[] public fraxPoolAddress;
+    address[] public poolAddress;
 
     // Mapping is also used for faster verification
-    mapping(address => bool) public isFraxPools;
+    mapping(address => bool) public isStablePools;
 
 
     uint256 public globalCollateralRatio; // 6 decimals of precision, e.g. 924102 = 0.924102
     uint256 public redemptionFee; // 6 decimals of precision, divide by 1000000 in calculations for fee
     uint256 public mintingFee; // 6 decimals of precision, divide by 1000000 in calculations for fee
-    uint256 public fraxStep; // Amount to change the collateralization ratio by upon refreshCollateralRatio()
+    uint256 public stableStep; // Amount to change the collateralization ratio by upon refreshCollateralRatio()
     uint256 public refreshCooldown; // Seconds to wait before being able to run refreshCollateralRatio() again
     uint256 public priceTarget; // The price of FRAX at which the collateral ratio will respond to; this value is only used for the collateral ratio mechanism and not for minting and redeeming which are hardcoded at $1
     uint256 public priceBand; // The bound above and below the price target at which the refreshCollateralRatio() will not change the collateral ratio
@@ -74,14 +70,14 @@ contract RStablecoin is ERC20Burnable, AbstractPausable {
     uint256 public lastCallTime; // Last time the refreshCollateralRatio function was called
 
     modifier onlyPools() {
-        require(isFraxPools[msg.sender] == true, "Only frax pools can call this function");
+        require(isStablePools[msg.sender] == true, "Only frax pools can call this function");
         _;
     }
 
     modifier onlyByOwnerGovernanceOrPool() {
         require(
             msg.sender == owner()
-            || isFraxPools[msg.sender] == true,
+            || isStablePools[msg.sender] == true,
             "Not the owner, the governance timelock, or a pool");
         _;
     }
@@ -93,7 +89,7 @@ contract RStablecoin is ERC20Burnable, AbstractPausable {
 
     ) public ERC20(_name, _symbol) AbstractPausable(_operatorMsg){
         _mint(msg.sender, GENESIS_SUPPLY);
-        fraxStep = 2500;
+        stableStep = 2500;
         // 6 decimals of precision, equal to 0.25%
         globalCollateralRatio = 1000000;
         // Frax system starts off fully collateralized (6 decimals of precision)
@@ -111,12 +107,12 @@ contract RStablecoin is ERC20Burnable, AbstractPausable {
         uint256 __eth_usd_price = uint256(ethUsdPricer.getLatestPrice()).mul(PRICE_PRECISION).div(uint256(10) ** ethUsdPricerDecimals);
         uint256 price_vs_eth = 0;
 
-        if (choice == PriceChoice.FRAX) {
-            price_vs_eth = uint256(fraxEthOracle.consult(weth, PRICE_PRECISION));
+        if (choice == PriceChoice.STABLE) {
+            price_vs_eth = uint256(stableEthOracle.consult(weth, PRICE_PRECISION));
             // How much FRAX if you put in PRICE_PRECISION WETH
         }
-        else if (choice == PriceChoice.FXS) {
-            price_vs_eth = uint256(fxsEthOracle.consult(weth, PRICE_PRECISION));
+        else if (choice == PriceChoice.STOCK) {
+            price_vs_eth = uint256(stockEthOracle.consult(weth, PRICE_PRECISION));
             // How much FXS if you put in PRICE_PRECISION WETH
         }
         else revert("INVALID PRICE CHOICE. Needs to be either 0 (FRAX) or 1 (FXS)");
@@ -127,12 +123,12 @@ contract RStablecoin is ERC20Burnable, AbstractPausable {
 
     // Returns X FRAX = 1 USD
     function stablePrice() public view returns (uint256) {
-        return oraclePrice(PriceChoice.FRAX);
+        return oraclePrice(PriceChoice.STABLE);
     }
 
     // Returns X FXS = 1 USD
     function stockPrice() public view returns (uint256) {
-        return oraclePrice(PriceChoice.FXS);
+        return oraclePrice(PriceChoice.STOCK);
     }
 
     function ethUsdPrice() public view returns (uint256) {
@@ -143,8 +139,8 @@ contract RStablecoin is ERC20Burnable, AbstractPausable {
     // It is cheaper gas-wise to just dump everything and only use some of the info
     function stableInfo() public view returns (uint256, uint256, uint256, uint256, uint256, uint256, uint256, uint256) {
         return (
-        oraclePrice(PriceChoice.FRAX), // frax_price()
-        oraclePrice(PriceChoice.FXS), // fxs_price()
+        oraclePrice(PriceChoice.STABLE), // frax_price()
+        oraclePrice(PriceChoice.STOCK), // fxs_price()
         totalSupply(), // totalSupply()
         globalCollateralRatio, // globalCollateralRatio()
         globalCollateralValue(), // globalCollateralValue
@@ -155,17 +151,17 @@ contract RStablecoin is ERC20Burnable, AbstractPausable {
     }
 
     function stablePoolAddressCount() public view returns (uint256) {
-        return (fraxPoolAddress.length);
+        return (poolAddress.length);
     }
 
     // Iterate through all frax pools and calculate all value of collateral in all pools globally 
     function globalCollateralValue() public view returns (uint256) {
         uint256 total_collateral_value_d18 = 0;
 
-        for (uint i = 0; i < fraxPoolAddress.length; i++) {
+        for (uint i = 0; i < poolAddress.length; i++) {
             // Exclude null addresses
-            if (fraxPoolAddress[i] != address(0)) {
-                total_collateral_value_d18 = total_collateral_value_d18.add(StablecoinPool(fraxPoolAddress[i]).collatDollarBalance());
+            if (poolAddress[i] != address(0)) {
+                total_collateral_value_d18 = total_collateral_value_d18.add(StablecoinPool(poolAddress[i]).collatDollarBalance());
             }
 
         }
@@ -177,23 +173,23 @@ contract RStablecoin is ERC20Burnable, AbstractPausable {
     // There needs to be a time interval that this can be called. Otherwise it can be called multiple times per expansion.
 
     function refreshCollateralRatio() public whenNotPaused {
-        uint256 frax_price_cur = stablePrice();
+        uint256 stablePriceCur = stablePrice();
         require(block.timestamp - lastCallTime >= refreshCooldown, "Must wait for the refresh cooldown since last refresh");
 
         // Step increments are 0.25% (upon genesis, changable by setStableStep())
 
-        if (frax_price_cur > priceTarget.add(priceBand)) {//decrease collateral ratio
-            if (globalCollateralRatio <= fraxStep) {//if within a step of 0, go to 0
+        if (stablePriceCur > priceTarget.add(priceBand)) {//decrease collateral ratio
+            if (globalCollateralRatio <= stableStep) {//if within a step of 0, go to 0
                 globalCollateralRatio = 0;
             } else {
-                globalCollateralRatio = globalCollateralRatio.sub(fraxStep);
+                globalCollateralRatio = globalCollateralRatio.sub(stableStep);
             }
-        } else if (frax_price_cur < priceTarget.sub(priceBand)) {//increase collateral ratio
-            if (globalCollateralRatio.add(fraxStep) >= 1000000) {
+        } else if (stablePriceCur < priceTarget.sub(priceBand)) {//increase collateral ratio
+            if (globalCollateralRatio.add(stableStep) >= 1000000) {
                 globalCollateralRatio = 1000000;
                 // cap collateral ratio at 1.000000
             } else {
-                globalCollateralRatio = globalCollateralRatio.add(fraxStep);
+                globalCollateralRatio = globalCollateralRatio.add(stableStep);
             }
         }
 
@@ -206,28 +202,27 @@ contract RStablecoin is ERC20Burnable, AbstractPausable {
     /* ========== RESTRICTED FUNCTIONS ========== */
 
     // Used by pools when user redeems
-    function poolBurnFrom(address b_address, uint256 b_amount) public onlyPools {
-        super.burnFrom(b_address, b_amount);
-        emit FRAXBurned(b_address, msg.sender, b_amount);
+    function poolBurnFrom(address _address, uint256 _amount) public onlyPools {
+        super.burnFrom(_address, _amount);
+        emit StableBurned(_address, msg.sender, _amount);
     }
 
-    function poolBurn(address b_address, uint256 b_amount) public onlyPools {
-        super.burn(b_amount);
-        emit FRAXBurned(b_address, msg.sender, b_amount);
+    function poolBurn(address _address, uint256 _amount) public onlyPools {
+        super.burn(_amount);
+        emit StableBurned(_address, msg.sender, _amount);
     }
 
-    // This function is what other frax pools will call to mint new FRAX 
-    function poolMint(address m_address, uint256 m_amount) public onlyPools {
-        super._mint(m_address, m_amount);
-        emit FRAXMinted(msg.sender, m_address, m_amount);
+    function poolMint(address _address, uint256 _amount) public onlyPools {
+        super._mint(_address, _amount);
+        emit StableMinted(msg.sender, _address, _amount);
     }
 
     // Adds collateral addresses supported, such as tether and busd, must be ERC20 
     function addPool(address pool_address) public onlyOwner {
         require(pool_address != address(0), "Zero address detected");
-        require(isFraxPools[pool_address] == false, "Address already exists");
-        isFraxPools[pool_address] = true;
-        fraxPoolAddress.push(pool_address);
+        require(isStablePools[pool_address] == false, "Address already exists");
+        isStablePools[pool_address] = true;
+        poolAddress.push(pool_address);
 
         emit PoolAdded(pool_address);
     }
@@ -235,15 +230,15 @@ contract RStablecoin is ERC20Burnable, AbstractPausable {
     // Remove a pool 
     function removePool(address pool_address) public onlyOwner {
         require(pool_address != address(0), "Zero address detected");
-        require(isFraxPools[pool_address] == true, "Address nonexistant");
+        require(isStablePools[pool_address] == true, "Address nonexistant");
 
         // Delete from the mapping
-        delete isFraxPools[pool_address];
+        delete isStablePools[pool_address];
 
         // 'Delete' from the array by setting the address to 0x0
-        for (uint i = 0; i < fraxPoolAddress.length; i++) {
-            if (fraxPoolAddress[i] == pool_address) {
-                fraxPoolAddress[i] = address(0);
+        for (uint i = 0; i < poolAddress.length; i++) {
+            if (poolAddress[i] == pool_address) {
+                poolAddress[i] = address(0);
                 // This will leave a null in the array and keep the indices the same
                 break;
             }
@@ -262,8 +257,8 @@ contract RStablecoin is ERC20Burnable, AbstractPausable {
     }
 
     function setStableStep(uint256 _new_step) public onlyOwner {
-        fraxStep = _new_step;
-        emit FraxStepSet(_new_step);
+        stableStep = _new_step;
+        emit StableStepSet(_new_step);
     }
 
     function setPriceTarget(uint256 _new_price_target) public onlyOwner {
@@ -278,8 +273,8 @@ contract RStablecoin is ERC20Burnable, AbstractPausable {
 
     function setStockAddress(address _fxs_address) public onlyOwner {
         require(_fxs_address != address(0), "Zero address detected");
-        fxsAddress = _fxs_address;
-        emit FXSAddressSet(_fxs_address);
+        stockAddress = _fxs_address;
+        emit StockAddressSet(_fxs_address);
     }
 
     function setETHUSDOracle(address _eth_usd_consumer_address) public onlyOwner {
@@ -298,22 +293,22 @@ contract RStablecoin is ERC20Burnable, AbstractPausable {
 
     function setStableEthOracle(address _frax_oracle_addr, address _weth_address) public onlyOwner {
         require((_frax_oracle_addr != address(0)) && (_weth_address != address(0)), "Zero address detected");
-        fraxEthOracleAddress = _frax_oracle_addr;
-        fraxEthOracle = UniswapPairOracle(_frax_oracle_addr);
+        stableEthOracleAddress = _frax_oracle_addr;
+        stableEthOracle = UniswapPairOracle(_frax_oracle_addr);
         weth = _weth_address;
 
-        emit FRAXETHOracleSet(_frax_oracle_addr, _weth_address);
+        emit StableETHOracleSet(_frax_oracle_addr, _weth_address);
     }
 
 
     function setStockEthOracle(address _fxs_oracle_addr, address _weth_address) public onlyOwner {
         require((_fxs_oracle_addr != address(0)) && (_weth_address != address(0)), "Zero address detected");
 
-        fxsEthOracleAddress = _fxs_oracle_addr;
-        fxsEthOracle = UniswapPairOracle(_fxs_oracle_addr);
+        stockEthOracleAddress = _fxs_oracle_addr;
+        stockEthOracle = UniswapPairOracle(_fxs_oracle_addr);
         weth = _weth_address;
 
-        emit FXSEthOracleSet(_fxs_oracle_addr, _weth_address);
+        emit StockEthOracleSet(_fxs_oracle_addr, _weth_address);
     }
 
 
