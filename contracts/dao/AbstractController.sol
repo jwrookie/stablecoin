cf5ef5fb538e4aa3795215d3b3877c8048b3f530
defi <defiiron@gmail.com>
2022-05-04 19:26:17 +0800

    Fix test bug

--- contracts/dao/AbstractController.sol
+++ contracts/dao/AbstractController.sol
@@ -13,14 +13,13 @@ import "../tools/CheckPermission.sol";
 import "../interface/IVeToken.sol";
 
 abstract contract AbstractController is CheckPermission {
+
+    using SafeMath for uint256;
+
     event Attach(address indexed owner, address indexed gauge, uint tokenId);
     event Detach(address indexed owner, address indexed gauge, uint tokenId);
     event Voted(address indexed voter, uint tokenId, uint weight);
     event Abstained(uint tokenId, uint weight);
-
-    //    uint internal immutable duration;
-
-
 
     uint public totalWeight; // total voting weight
 
@@ -29,9 +28,8 @@ abstract contract AbstractController is CheckPermission {
     address internal immutable boost;
 
     mapping(address => uint) public weights; // pool => weight
-    mapping(uint => mapping(address => uint)) public votes; // nft => pool => votes
-    mapping(uint => address[]) public poolVote; // nft => pools
     mapping(uint => uint) public usedWeights;  // nft => total voting weight of user
+    mapping(uint => address) public userPool;  // nft => pool voting weight of user
 
     address [] public poolInfo;
 
@@ -41,10 +39,6 @@ abstract contract AbstractController is CheckPermission {
         boost = _boost;
     }
 
-    function getPoolVote(uint256 tokenId) public view returns (address[] memory){
-        return poolVote[tokenId];
-    }
-
     function reset(uint _tokenId) external {
         require(IVeToken(veToken).isApprovedOrOwner(msg.sender, _tokenId));
         _reset(_tokenId);
@@ -52,86 +46,40 @@ abstract contract AbstractController is CheckPermission {
     }
 
     function _reset(uint _tokenId) internal {
-        address[] storage _poolVote = poolVote[_tokenId];
-        uint _poolVoteCnt = _poolVote.length;
-        uint _totalWeight = 0;
-        for (uint i = 0; i < _poolVoteCnt; i ++) {
-            address _pool = _poolVote[i];
-            uint _votes = votes[_tokenId][_pool];
-
-            if (_votes != 0) {
-                _updatePoolInfo(_pool);
-                weights[_pool] -= _votes;
-                votes[_tokenId][_pool] -= _votes;
-                if (_votes > 0) {
-                    _totalWeight += _votes;
-                } else {
-                    _totalWeight -= _votes;
-                }
-                emit Abstained(_tokenId, _votes);
-            }
-        }
+        uint _totalWeight = usedWeights[_tokenId];
+        address _pool = userPool[_tokenId];
+        emit Abstained(_tokenId, _totalWeight);
         totalWeight -= uint256(_totalWeight);
         usedWeights[_tokenId] = 0;
-        delete poolVote[_tokenId];
+        delete userPool[_tokenId];
     }
 
-    function _vote(uint _tokenId, address[] memory _poolVote, uint[] memory _weights) internal {
+    function _vote(uint _tokenId, address _poolVote) internal {
         _reset(_tokenId);
-        uint _poolCnt = _poolVote.length;
         uint _weight = IVeToken(veToken).balanceOfNFT(_tokenId);
-        uint _totalVoteWeight = 0;
+        uint _totalVoteWeight = _weight;
         uint _totalWeight = 0;
         uint _usedWeight = 0;
 
-        for (uint i = 0; i < _poolCnt; i++) {
-            _totalVoteWeight = _weights[i] > 0 ? _totalVoteWeight + _weights[i] : _totalVoteWeight - _weights[i];
-        }
 
-        for (uint i = 0; i < _poolCnt; i++) {
-            address _pool = _poolVote[i];
+        weights[_poolVote] = weights[_poolVote].add(_weight);
 
+        emit Voted(msg.sender, _tokenId, _weight);
 
-            uint _poolWeight = _weights[i] * _weight / _totalVoteWeight;
-            require(votes[_tokenId][_pool] == 0, "token pool is 0");
-            require(_poolWeight != 0, "weight is 0");
-            _updatePoolInfo(_pool);
-
-            poolVote[_tokenId].push(_pool);
-
-            weights[_pool] += _poolWeight;
-            votes[_tokenId][_pool] += _poolWeight;
-            if (_poolWeight > 0) {
-            } else {
-                _poolWeight = _poolWeight - _poolWeight;
-            }
-            _usedWeight += _poolWeight;
-            _totalWeight += _poolWeight;
-            emit Voted(msg.sender, _tokenId, _poolWeight);
-
-        }
-        if (_usedWeight > 0) IVeToken(veToken).voting(_tokenId);
-        totalWeight += uint256(_totalWeight);
-        usedWeights[_tokenId] = uint256(_usedWeight);
+        if (_weight > 0) IVeToken(veToken).voting(_tokenId);
+        totalWeight += _weight;
+        usedWeights[_tokenId] = _weight;
+        //        _updatePoolInfo(_pool);
     }
 
     function poke(uint _tokenId) external {
-        address[] memory _poolVote = poolVote[_tokenId];
-        uint _poolCnt = _poolVote.length;
-        uint[] memory _weights = new uint[](_poolCnt);
-
-        for (uint i = 0; i < _poolCnt; i ++) {
-            _weights[i] = votes[_tokenId][_poolVote[i]];
-        }
-
-        _vote(_tokenId, _poolVote, _weights);
+        _vote(_tokenId, userPool[_tokenId]);
     }
 
 
-    function vote(uint tokenId, address[] calldata _poolVote, uint[] calldata _weights) external {
+    function vote(uint tokenId, address _poolVote) external {
         require(IVeToken(veToken).isApprovedOrOwner(msg.sender, tokenId));
-        require(_poolVote.length == _weights.length);
-        _vote(tokenId, _poolVote, _weights);
+        _vote(tokenId, _poolVote);
     }
 
     function _updatePoolInfo(address _pool) internal {
