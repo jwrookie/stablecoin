097fb92ac113ae355f459eaa8d99d54239a7f0bb
JunkingBoy <junkingboy@163.com>
2022-05-17 19:18:12 +0800

    Modify function

--- test/Tools/Deploy.js
+++ test/Tools/Deploy.js
@@ -5,16 +5,16 @@ const GetConfigAboutCRV = async (user) => {
     let resultArray = new Array();
 
     if (ZEROADDRESS === user.address || undefined === user.address) {
-        return Error("Please enter the correct user address!");
+        throw "Please enter the correct user address!";
     }
 
-    weth = await Weth(user);
-    factory = await Factory(user);
-    router = await Router(user, factory, weth);
-    registry = await Registry(user);
-    poolRegistry = await PoolRegistry(user, registry);
-    crvFactory = await CRVFactory(user, registry);
-    plain3Balances = await Plain3Balances(user);
+    let weth = await Weth(user);
+    let factory = await Factory(user);
+    let router = await Router(user, factory, weth);
+    let registry = await Registry(user);
+    let poolRegistry = await PoolRegistry(user, registry);
+    let crvFactory = await CRVFactory(user, registry);
+    let plain3Balances = await Plain3Balances(user);
 
     resultArray.push(weth, factory, router, registry, poolRegistry, crvFactory, plain3Balances);
 
