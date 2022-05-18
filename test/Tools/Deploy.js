4f1d374052a7a5a017854305eab4934a778f3017
JunkingBoy <junkingboy@163.com>
2022-05-18 21:29:15 +0800

    Fixing reference issues

--- test/Tools/Deploy.js
+++ test/Tools/Deploy.js
@@ -1,4 +1,4 @@
-const {Weth,Factory,Router,Registry,PoolRegistry,CRVFactory,Plain3Balances,SetPoolByCrvFactory} = require("../Core/LibSourceConfig");
+const {Weth,Factory,Router,Registry,PoolRegistry,CRVFactory,Plain3Balances,SetPoolByCrvFactory, SetPlainImplementations} = require("../Core/LibSourceConfig");
 const {ZEROADDRESS} = require("../Lib/Address");
 
 const GetConfigAboutCRV = async (user) => {
