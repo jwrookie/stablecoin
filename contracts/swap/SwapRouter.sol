64f6488fcb642e5a32517af091fa105f16dcceed
defi <defiiron@gmail.com>
2022-05-19 15:03:47 +0800

    Merge branch 'master' into feature/amo
    # Conflicts:
    #	contracts/swap/SwapRouter.sol
    #	scripts/frax.js
    #	test/TestBoost.js
    #	test/swapController.test.js
    #	test/test_pool5coins_boost_vote.js

--- contracts/swap/SwapRouter.sol
+++ contracts/swap/SwapRouter.sol
@@ -15,12 +15,12 @@ import "../interface/ISwapMining.sol";
 contract SwapRouter is CheckPermission {
     event ChangeSwapMining(address indexed oldSwapMining, address indexed newSwapMining);
 
-    address public WETH;
+    address public weth;
 
     address public swapMining;
 
     constructor(address _operatorMsg, address _weth) CheckPermission(_operatorMsg){
-        WETH = _weth;
+        weth = _weth;
     }
 
     modifier ensure(uint256 deadline) {
@@ -35,7 +35,7 @@ contract SwapRouter is CheckPermission {
         emit ChangeSwapMining(oldSwapMining, swapMining);
     }
 
-    function callStableSwapMining(
+    function _callStableSwapMining(
         address account,
         address pair,
         uint256 i,
@@ -61,7 +61,7 @@ contract SwapRouter is CheckPermission {
         }
     }
 
-    function callCryptoSwapMining(
+    function _callCryptoSwapMining(
         address account,
         address pair,
         uint256 i,
@@ -87,7 +87,7 @@ contract SwapRouter is CheckPermission {
         }
     }
 
-    function callCryptoTokenSwapMining(
+    function _callCryptoTokenSwapMining(
         address account,
         address pair,
         uint256 i,
@@ -106,29 +106,29 @@ contract SwapRouter is CheckPermission {
         address pool,
         uint256 from,
         uint256 to,
-        uint256 _from_amount,
-        uint256 _min_to_amount,
+        uint256 fromAmount,
+        uint256 minToAmount,
         address receiver,
         uint256 deadline
     ) external ensure(deadline) {
         int128 fromInt = int128(uint128(from));
         int128 toInt = int128(uint128(to));
         address fromToken = IStablePool(pool).coins(from);
-        address toToken = IStablePool(pool).coins(to);
-        if (IERC20(fromToken).allowance(address(this), pool) < _from_amount) {
+        //        address toToken = IStablePool(pool).coins(to);
+        if (IERC20(fromToken).allowance(address(this), pool) < fromAmount) {
             TransferHelper.safeApprove(fromToken, pool, type(uint256).max);
         }
-        TransferHelper.safeTransferFrom(fromToken, msg.sender, address(this), _from_amount);
-        IStablePool(pool).exchange(fromInt, toInt, _from_amount, _min_to_amount, receiver);
-        callStableSwapMining(receiver, pool, from, _from_amount);
+        TransferHelper.safeTransferFrom(fromToken, msg.sender, address(this), fromAmount);
+        IStablePool(pool).exchange(fromInt, toInt, fromAmount, minToAmount, receiver);
+        _callStableSwapMining(receiver, pool, from, fromAmount);
     }
 
     function swapMeta(
         address pool,
         uint256 from,
         uint256 to,
-        uint256 _from_amount,
-        uint256 _min_to_amount,
+        uint256 fromAmount,
+        uint256 minToAmount,
         address receiver,
         uint256 deadline
     ) external ensure(deadline) {
@@ -143,73 +143,73 @@ contract SwapRouter is CheckPermission {
             callStable = 1;
         }
 
-        if (IERC20(fromToken).allowance(address(this), pool) < _from_amount) {
+        if (IERC20(fromToken).allowance(address(this), pool) < fromAmount) {
             TransferHelper.safeApprove(fromToken, pool, type(uint256).max);
         }
 
-        TransferHelper.safeTransferFrom(fromToken, msg.sender, address(this), _from_amount);
-        IStablePool(pool).exchange_underlying(fromInt, toInt, _from_amount, _min_to_amount, receiver);
-        callStableSwapMining(receiver, pool, callStable, _from_amount);
+        TransferHelper.safeTransferFrom(fromToken, msg.sender, address(this), fromAmount);
+        IStablePool(pool).exchange_underlying(fromInt, toInt, fromAmount, minToAmount, receiver);
+        _callStableSwapMining(receiver, pool, callStable, fromAmount);
     }
 
     function swapToken(
         address pool,
         uint256 from,
         uint256 to,
-        uint256 _from_amount,
-        uint256 _min_to_amount,
+        uint256 fromAmount,
+        uint256 minToAmount,
         address receiver,
         uint256 deadline
     ) external ensure(deadline) {
         address fromToken = IStablePool(pool).coins(from);
-        address toToken = IStablePool(pool).coins(to);
-        if (IERC20(fromToken).allowance(address(this), pool) < _from_amount) {
+        //        address toToken = IStablePool(pool).coins(to);
+        if (IERC20(fromToken).allowance(address(this), pool) < fromAmount) {
             TransferHelper.safeApprove(fromToken, pool, type(uint256).max);
         }
-        TransferHelper.safeTransferFrom(fromToken, msg.sender, address(this), _from_amount);
-        ICryptoPool(pool).exchange(from, to, _from_amount, _min_to_amount, false, receiver);
-        callCryptoSwapMining(receiver, pool, from, _from_amount);
+        TransferHelper.safeTransferFrom(fromToken, msg.sender, address(this), fromAmount);
+        ICryptoPool(pool).exchange(from, to, fromAmount, minToAmount, false, receiver);
+        _callCryptoSwapMining(receiver, pool, from, fromAmount);
     }
 
     function swapCryptoToken(
         address pool,
         uint256 from,
         uint256 to,
-        uint256 _from_amount,
-        uint256 _min_to_amount,
+        uint256 fromAmount,
+        uint256 minToAmount,
         address receiver,
         uint256 deadline
     ) external ensure(deadline) {
         address fromToken = IZapDepositor4pool(pool).underlying_coins(from);
-        if (IERC20(fromToken).allowance(address(this), pool) < _from_amount) {
+        if (IERC20(fromToken).allowance(address(this), pool) < fromAmount) {
             TransferHelper.safeApprove(fromToken, pool, type(uint256).max);
         }
-        TransferHelper.safeTransferFrom(fromToken, msg.sender, address(this), _from_amount);
-        IZapDepositor4pool(pool).exchange_underlying(from, to, _from_amount, _min_to_amount, receiver);
-        callCryptoTokenSwapMining(receiver, pool, from, _from_amount);
+        TransferHelper.safeTransferFrom(fromToken, msg.sender, address(this), fromAmount);
+        IZapDepositor4pool(pool).exchange_underlying(from, to, fromAmount, minToAmount, receiver);
+        _callCryptoTokenSwapMining(receiver, pool, from, fromAmount);
     }
 
     function swapEthForToken(
         address pool,
         uint256 from,
         uint256 to,
-        uint256 _from_amount,
-        uint256 _min_to_amount,
+        uint256 fromAmount,
+        uint256 minToAmount,
         address receiver,
         uint256 deadline
     ) external payable ensure(deadline) {
         uint256 bal = msg.value;
         address fromToken = IStablePool(pool).coins(from);
-        if (fromToken != WETH) {
-            if (IERC20(fromToken).allowance(address(this), pool) < _from_amount) {
+        if (fromToken != weth) {
+            if (IERC20(fromToken).allowance(address(this), pool) < fromAmount) {
                 TransferHelper.safeApprove(fromToken, pool, type(uint256).max);
             }
 
-            TransferHelper.safeTransferFrom(fromToken, msg.sender, address(this), _from_amount);
+            TransferHelper.safeTransferFrom(fromToken, msg.sender, address(this), fromAmount);
         }
 
-        ICryptoPool(pool).exchange{value : bal}(from, to, _from_amount, _min_to_amount, true, receiver);
-        callCryptoSwapMining(receiver, pool, from, _from_amount);
+        ICryptoPool(pool).exchange{value : bal}(from, to, fromAmount, minToAmount, true, receiver);
+        _callCryptoSwapMining(receiver, pool, from, fromAmount);
     }
 
     function recoverERC20(address _tokenAddress, uint256 _tokenAmount) external onlyOperator {
