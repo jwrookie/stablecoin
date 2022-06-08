6f9416bc4bc3a5dc8584302caed0ae633dae515a
simple7616 <simple7616@gmail.com>
2022-06-08 16:06:30 +0800

    feat: coverage

--- test/test_stablecoin/test_stablecoin.js
+++ test/test_stablecoin/test_stablecoin.js
@@ -6,12 +6,16 @@ const {toWei, fromWei, toBN} = require("web3-utils");
 const PancakeFactory = require("../../test/mock/PancakeFactory.json");
 const PancakeRouter = require("../mock/PancakeRouter.json");
 const PancakePair = require("../mock/PancakePair.json");
+const {time} = require("@openzeppelin/test-helpers");
+const {ethers} = require("hardhat");
+const {BigNumber} = require("ethers");
 
 contract('StableCoin', async () => {
 
     beforeEach(async () => {
+        [owner, dev, addr1] = await ethers.getSigners();
+
         const {
-            owner, dev, zeroAddress,
             Stock, RStablecoin, PoolLibrary,
             CheckPermission, Operatable,
             TestOracle, MockChainLink, ChainlinkETHUSDPriceConsumer
@@ -27,7 +31,7 @@ contract('StableCoin', async () => {
         await frax.setStockAddress(fxs.address);
 
         [usdc] = await $.mockTokenBatch(18, 0, "usdc");
-        await usdc.mint(owner.address, toWei("1000000"));
+        await usdc.mint(owner.address, toWei("1000000000"));
 
         weth = await $.deploy(owner, WETH9.bytecode, WETH9.abi);
         await weth.deposit({value: toWei("100")});
@@ -36,8 +40,9 @@ contract('StableCoin', async () => {
         stablecoinPool = await Config.getStablecoinPool(poolLibrary, checkPermission, frax, fxs, usdc);
 
         await frax.addPool(stablecoinPool.address);
+        await fxs.addPool(stablecoinPool.address);
 
-        pancakeFactory = await $.deploy(owner, PancakeFactory.bytecode, PancakeFactory.abi, [owner.address]);
+        const pancakeFactory = await $.deploy(owner, PancakeFactory.bytecode, PancakeFactory.abi, [owner.address]);
         await pancakeFactory.createPair(usdc.address, weth.address);
         await pancakeFactory.createPair(fxs.address, weth.address);
         await pancakeFactory.createPair(frax.address, weth.address);
@@ -55,24 +60,25 @@ contract('StableCoin', async () => {
         let _deadline = new Date().getTime() + 1000;
         await pancakeRouter.addLiquidity(
             usdc.address, weth.address,
-            toWei("1"), toWei("1"), 0, 0,
+            toWei("10"), toWei("10"), 0, 0,
             owner.address, _deadline
         );
         await pancakeRouter.addLiquidity(
             fxs.address, weth.address,
-            toWei("1"), toWei("1"), 0, 0,
+            toWei("10"), toWei("10"), 0, 0,
             owner.address, _deadline
         );
         await pancakeRouter.addLiquidity(
             frax.address, weth.address,
-            toWei("1"), toWei("1"), 0, 0,
+            toWei("10"), toWei("10"), 0, 0,
             owner.address, _deadline
         );
 
         chainLink = await MockChainLink.deploy();
         ETHUSDOracle = await ChainlinkETHUSDPriceConsumer.deploy(chainLink.address);
 
-        // chainLink.setAnswer(1e18);
+        await chainLink.setAnswer(toWei("1"));
+
         await frax.setETHUSDOracle(ETHUSDOracle.address);
 
         fxsEthOracle = await Config.getUniswapPairOracle(pancakeFactory, fxs, weth);
@@ -80,24 +86,92 @@ contract('StableCoin', async () => {
 
         fraxEthOracle = await Config.getUniswapPairOracle(pancakeFactory, frax, weth);
         await frax.setStableEthOracle(fraxEthOracle.address, weth.address);
+
+        usdcEthOracle = await Config.getUniswapPairOracle(pancakeFactory, usdc, weth);
+        await stablecoinPool.setCollatETHOracle(usdcEthOracle.address, weth.address);
     });
 
-    it('test pair', async () => {
-        usdc_weth_pair = await pancakeFactory.getPair(usdc.address, weth.address);
-        usdc_weth = await pancakePair.attach(usdc_weth_pair);
+    // it('test pair', async () => {
+    //     let usdc_weth_pair = await pancakeFactory.getPair(usdc.address, weth.address);
+    //     let usdc_weth = await pancakePair.attach(usdc_weth_pair);
+    //
+    //     expect(usdc_weth_pair).to.be.eq(usdc_weth.address);
+    //
+    //
+    //     let fxs_weth_pair = await pancakeFactory.getPair(fxs.address, weth.address);
+    //     let fxs_weth = await pancakePair.attach(fxs_weth_pair);
+    //
+    //     expect(fxs_weth_pair).to.be.eq(fxs_weth.address);
+    //
+    //
+    //     let frax_weth_pair = await pancakeFactory.getPair(frax.address, weth.address);
+    //     let frax_weth = await pancakePair.attach(frax_weth_pair);
+    //
+    //     expect(frax_weth_pair).to.be.eq(frax_weth.address);
+    // });
 
-        expect(usdc_weth_pair).to.be.eq(usdc_weth.address);
+    it('test refreshCR', async () => {
+        let stablePriceBef = await frax.stablePrice();
+        let CRBef = await frax.globalCollateralRatio();
+
+        let _deadline = new Date().getTime() + 1000;
+        await pancakeRouter.swapExactTokensForTokens(
+            toWei('1'),
+            0,
+            [weth.address, frax.address],
+            owner.address,
+            _deadline
+        );
+        await time.increase(time.duration.hours(1));
+        await fraxEthOracle.update();
+        await fxsEthOracle.update();
+        await usdcEthOracle.update();
+        await frax.refreshCollateralRatio();
+
+        let stablePriceAft = await frax.stablePrice();
+        let CRAft = await frax.globalCollateralRatio();
+
+        expect(stablePriceAft).to.be.gt(stablePriceBef);
+        expect(CRAft).to.be.eq(BigNumber.from(CRBef).sub(2500));
 
 
-        fxs_weth_pair = await pancakeFactory.getPair(fxs.address, weth.address);
-        fxs_weth = await pancakePair.attach(fxs_weth_pair);
+        await usdc.approve(stablecoinPool.address, toWei("20000000"));
+        await fxs.approve(stablecoinPool.address, toWei("2000000"));
+        await frax.approve(stablecoinPool.address, toWei("10000000"));
 
-        expect(fxs_weth_pair).to.be.eq(fxs_weth.address);
+        expect(await frax.totalSupply()).eq(toWei("2000000"));
 
+        await time.increase(time.duration.hours(1));
+        await stablecoinPool.mintFractionalStable(toWei("10000000"), toWei("1000000"), 0);
+        // expect(await frax.totalSupply()).eq(toWei("2000100.2506265664160401"));
+        await fraxEthOracle.update();
+        await fxsEthOracle.update();
+        await usdcEthOracle.update();
+        // console.log(fromWei(toBN(await frax.totalSupply())));
+        // console.log(fromWei(toBN(await frax.lastQX())));
+        // console.log(await frax.refesh())
+        await frax.refreshCollateralRatio();
+        console.log(await frax.maxCR());
 
-        frax_weth_pair = await pancakeFactory.getPair(frax.address, weth.address);
-        frax_weth = await pancakePair.attach(frax_weth_pair);
+        // console.log(await frax.globalCollateralRatio());
 
-        expect(frax_weth_pair).to.be.eq(frax_weth.address);
+        await time.increase(time.duration.hours(1));
+        await stablecoinPool.redeemFractionalStable(toWei("10000"), 0, 0);
+        // expect(await frax.totalSupply()).eq(toWei("2000090.2506265664160401"));
+        await fraxEthOracle.update();
+        await fxsEthOracle.update();
+        await usdcEthOracle.update();
+        // console.log(fromWei(toBN(await frax.totalSupply())));
+        // console.log(fromWei(toBN(await frax.lastQX())));
+        // console.log(await frax.refesh())
+        await frax.refreshCollateralRatio();
+        console.log(await frax.maxCR());
+
+        // console.log(await frax.globalCollateralRatio());
     });
+
+    // it('test K', async () => {
+    //     await frax.setKAndKDuration(1e3, toWei("10000000"));
+    //     console.log(await frax.maxCR());
+    // });
 });
